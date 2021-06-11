6dd31d901fe637e74468ac930587cf13d6d5ed54
Thomas Taylor <t.h.taylor@outlook.com>
2021-06-11 13:23:54 +0100

    Add files via upload
    Time max for less that 999

--- Working with dplyr.R
+++ Working with dplyr.R
@@ -3,3 +3,5 @@ setwd("C:/Users/owner/Documents/Uni stuff/PhD/R scripts/Chapter 1/Script for ide
 load("temp to experiment with dplyr.Rda")
 
 temp1<-temp%>%group_by(sim)%>%slice_head(n=6)%>%filter(time == max(time))
+
+temptimemax<-temp%>%filter(infected<998)%>%filter(time==max(time))
