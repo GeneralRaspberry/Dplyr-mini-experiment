9183a5d432790fa63c2f7b996187da53022b63e2
Thomas Taylor <t.h.taylor@outlook.com>
2021-06-11 13:19:49 +0100

    Add files via upload
    Added a filter command that takes max, to do so, simply make your filter == the max value of what you desire.

--- Working with dplyr.R
+++ Working with dplyr.R
@@ -2,4 +2,4 @@ getwd()
 setwd("C:/Users/owner/Documents/Uni stuff/PhD/R scripts/Chapter 1/Script for identifying parameter space/Workspace objects")
 load("temp to experiment with dplyr.Rda")
 
-temp1<-temp%>%group_by(sim)%>%slice_head(n=6)
+temp1<-temp%>%group_by(sim)%>%slice_head(n=6)%>%filter(time == max(time))
