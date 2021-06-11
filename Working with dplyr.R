922db518e010ec688f9cdf6b8032c74eb92e5dc6
Thomas Taylor <t.h.taylor@outlook.com>
2021-06-11 12:55:33 +0100

    Add files via upload
    Initial call to work directory to load temp data.frame

---
+++ Working with dplyr.R
@@ -0,0 +1,4 @@
+getwd()
+setwd("C:/Users/owner/Documents/Uni stuff/PhD/R scripts/Chapter 1/Script for identifying parameter space/
+      Workspace objects")
+load("temp to experiment with dplyr.Rda")
